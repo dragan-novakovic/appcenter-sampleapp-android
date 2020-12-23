852f2727f105a12e56f48ee3595c885ce0d11654
miroslavmatovic <v-mirosm@microsoft.com>
2020-12-23 11:49:36 +0100

    Use random from current time

--- app/src/main/java/ms/appcenter/sampleapp/android/WelcomeActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/WelcomeActivity.java
@@ -1,17 +1,19 @@
 package ms.appcenter.sampleapp.android;
 
 import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
-import java.util.Random;
+import java.time.LocalDateTime;
 
 public class WelcomeActivity extends Fragment {
     @Override
@@ -28,15 +30,14 @@ public class WelcomeActivity extends Fragment {
     }
 
     public static class MyCrashDialog extends DialogFragment {
-        Random rand = new Random();
+        @RequiresApi(api = Build.VERSION_CODES.O)
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Generate random integers in range 0 to 9
-            int rand_int = rand.nextInt(50);
-            System.out.println("Random Integers: "+rand_int);
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage("A crash report will be sent when you reopen the app.")
-                    .setPositiveButton("Crash app (" + String.valueOf(rand_int) + ")", (dialog, id) -> {
-                        throw new RuntimeException("crashing " + String.valueOf(rand_int));
+                    .setPositiveButton("Crash app", (dialog, id) -> {
+                        int seconds = LocalDateTime.now().getSecond();
+                        System.out.println("Random seconds: "+seconds);
+                        throw new RuntimeException("crashing " + String.valueOf(seconds));
                     }).setNegativeButton("Cancel", (dialog, id) -> {
                 // Add any code you'd like to execute when users click "Cancel"
             });
